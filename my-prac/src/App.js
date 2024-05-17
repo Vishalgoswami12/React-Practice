c3aea10e01b33b45d7a104ac185d2be30bbc7512
Vishal Goswami <vishalgoswami2250@gmail.com>
2024-05-17 11:38:46 +0530

    router managing

--- my-prac/src/App.js
+++ my-prac/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import React from "react";
 import Timer from './Components/Timer';
 import DisplayData from './Components/DisplayData';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 function App() {
  
   return (
      <div className="App">
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<Timer/>}></Route>
+        </Routes>
+      </Router> */}
       <Timer/>
-     <DisplayData/>
+     {/* <DisplayData/> */}
     </div>
   );
 }
--- my-prac/src/Components/DisplayData.js
+++ my-prac/src/Components/DisplayData.js
@@ -1,40 +1,50 @@
-import React,{useState,useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
-function DisplayData(){
-    const [data, setData] = useState([]);
-    const [display, setDisplay] = useState(5);
-    const [active, setActive] = useState(false);
-    const handleButton = (e) => {
-      if ((e.target.innerText = "Show More")) {
-        setDisplay(data.length);
-      } else {
-        setDisplay(display);
-        setActive(false);
-      }
-    };
-    useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/posts/")
-        .then((res) => res.json())
-        .then((data) => setData(data));
-    }, []);
-    return(
-        <>
-          {data &&
-        data.slice(0, display).map((ele) => {
+function DisplayData() {
+  const [data, setData] = useState([]);
+  const [display, setDisplay] = useState(5);
+  const [active, setActive] = useState(false);
+  const [show, setShow] = useState(null);
+  const[input,setInput] = useState("")
+
+  const handleButton = () => {
+    if (!active) {
+      setDisplay(data.length);
+    } else {
+      setDisplay(5);
+    }
+    setActive(!active);
+  };
+
+  const handleChange = (id) => {
+    setShow(show === id ? null : id);
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <>
+    <input type="text" value={input} placeholder="search" onChange={(e) => setInput(e.target.value)}/>
+      {data &&
+        data.slice(0, display).filter((ele) => ele.title.toLowerCase().includes(input.toLocaleLowerCase()) || ele.body.toLowerCase().includes(input.toLowerCase()) ).map((ele) => {
           const { id, title, body } = ele;
           return (
-            <div>
+            <div key={id} onClick={() => handleChange(id)}>
               <h2>{id}</h2>
               <h3>{title}</h3>
-              <p>{body}</p>
+              {show === id ? <p>{body}</p> : ""}
             </div>
           );
         })}
-         <button onClick={handleButton}>
+      <button onClick={handleButton}>
         {active ? "Show Less" : "Show More"}
       </button>
-        </>
-    )
+    </>
+  );
 }
 
 export default DisplayData;
